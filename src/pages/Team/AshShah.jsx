9ef155b20961456eaa46f09ff4f087c8be1fbff4
RaviSharma78 <ravi@crackerdigital.com>
2026-02-19 15:50:28 +0530

    perf: Optimize LCP by preloading hero images, moving font and icon imports to index.html, and updating image referencing paths.

--- src/pages/Team/AshShah.jsx
+++ src/pages/Team/AshShah.jsx
@@ -7,8 +7,9 @@ import Footer from "../../components/Footer/Footer";
 import FooterCTA from "../../components/Footer/FooterCTA";
 
 import ashShahImg from "../../assets/images/AshShah.webp";
-import heroSectionImage from "../../assets/images/heroSetionImage.webp";
 import StructuredData from "../../components/StructuredData";
+
+const heroSectionImage = "/assets/images/heroSetionImage.webp";
 
 const AshShah = () => {
   /* Scroll Reveal Animation */
