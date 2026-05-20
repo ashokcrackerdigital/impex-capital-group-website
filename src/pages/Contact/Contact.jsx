542e058289186443941393b86c1340d43124b59d
ashokcrackerdigital <ashok@crackerdigital.com>
2026-05-20 12:48:46 +0530

    updatedAddress

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -245,7 +245,7 @@ const Contact = () => {
           {
             question: "Where is Impex Capital Group located?",
             answer:
-              "Impex Capital Group is located at 5251 Westheimer Suite 925, Houston, TX 77056."
+              "Impex Capital Group is located at 5251 Westheimer Rd Suite 925, Houston, TX 77056."
           }
         ]}
       />
