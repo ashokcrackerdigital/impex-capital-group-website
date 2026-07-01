dfc9072aabce52e0ae33b4beca10be305844b1e4
ashokcrackerdigital <ashok@crackerdigital.com>
2026-07-01 17:10:05 +0530

    hide img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ import SEO from "../../components/SEO";
 
 import heroVideo from "../../assets/videos/opt_Luxury_Apartment_Drone_Footage.mp4";
 import seniorLivingImg from "../../assets/images/Seniorliving.webp";
-import financialSolutionsImg from "../../assets/images/financialSolutionsSectionImg.png";
+// import financialSolutionsImg from "../../assets/images/financialSolutionsSectionImg.png";
 // import topInvestorImg from "../../assets/images/topinvestor.webp";
 // import bottomInvestorImg from "../../assets/images/Bottominvestor.webp";
 import StructuredData from "../../components/StructuredData";
